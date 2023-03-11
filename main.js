8590d24d32e3fffe8f989b5c7186e5702f0d18d1
Fakher Al Deen <fakheralden1@gmail.com>
2023-03-12 00:27:09 +0300

    boobz

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var cors = require('cors');
+app.use(express.json());
+app.use(cors(corsOptions));
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // For legacy browser support
+}
 
 app.get('/tezk', (req, res) => {
     res.send(req.body)
